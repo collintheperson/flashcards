19c56952305eb454750e74832d0ab209c072a084
Collin Person and Stephanie Mayer <collin@>
2017-07-05 10:16:46 -0700

    added bootstrap,colomns,js

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(function() {
+  $(".clickable").click(function() {
+    $("#js").toggle();
+    $("#js1").toggle();
+  });
+  $(".clickable1").click(function() {
+    $("#op").toggle();
+    $("#op1").toggle();
+  });
+  $(".clickable2").click(function() {
+    $("#var").toggle();
+    $("#var1").toggle();
+    });
+  $(".clickable3").click(function() {
+    $("#vn").toggle();
+    $("#vn1").toggle();
+    });
+  $(".clickable4").click(function() {
+    $("#fun").toggle();
+    $("#fun1").toggle();
+  });
+  $(".clickable5").click(function() {
+    $("#met").toggle();
+    $("#met1").toggle();
+  });
+});
