59e211da7b1d303b9bd137191a68f0601820914b
Collin Person and Stephanie Mayer <stephanie@>
2017-07-05 10:47:02 -0700

    added color and divs

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,52 @@ $(function() {
     $("#met").toggle();
     $("#met1").toggle();
   });
+  $(".clickable6").click(function() {
+    $("#arg").toggle();
+    $("#arg1").toggle();
+  });
+  $(".clickable7").click(function() {
+    $("#par").toggle();
+    $("#par1").toggle();
+  });
+  $(".clickable8").click(function() {
+    $("#ret").toggle();
+    $("#ret1").toggle();
+  });
+  $(".clickable9").click(function() {
+    $("#cha").toggle();
+    $("#cha1").toggle();
+  });
+  $(".clickable10").click(function() {
+    $("#str").toggle();
+    $("#str1").toggle();
+  });
+  $(".clickable11").click(function() {
+    $("#boo").toggle();
+    $("#boo1").toggle();
+  });
+  $(".clickable12").click(function() {
+    $("#und").toggle();
+    $("#und1").toggle();
+  });
+  $(".clickable13").click(function() {
+    $("#nan").toggle();
+    $("#nan1").toggle();
+  });
+  $(".clickable14").click(function() {
+    $("#esc").toggle();
+    $("#esc1").toggle();
+  });
+  $(".clickable15").click(function() {
+    $("#ale").toggle();
+    $("#ale1").toggle();
+  });
+  $(".clickable16").click(function() {
+    $("#com").toggle();
+    $("#com1").toggle();
+  });
+  $(".clickable17").click(function() {
+    $("#cli").toggle();
+    $("#cli1").toggle();
+  });
 });
